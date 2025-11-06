679628c1f44ad3663156ae84feaa79a24ac550d1
Eero Vilpponen <eero.vilpponen@gmail.com>
2025-11-06 07:56:16 +0200

    Update

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
-import time
 import pychromecast
-import zeroconf
+import dotenv
 
-# Create a browser which prints the friendly name of found chromecast devices
-zconf = zeroconf.Zeroconf()
-browser = pychromecast.CastBrowser(pychromecast.SimpleCastListener(lambda uuid, service: print(browser.devices[uuid].friendly_name)), zconf)
-browser.start_discovery()
-# Shut down discovery
-pychromecast.discovery.stop_discovery(browser)
+dotenv.load_dotenv()
 
-name = "Kitchen display"
+name = dotenv.get_key(dotenv.find_dotenv(), "MAIN_CHROMECAST_NAME")
+base_url = dotenv.get_key(dotenv.find_dotenv(), "BASE_URL")
 
-# Discover and connect to chromecasts named Living Room
+# Discover and connect to chromecasts named Kitchen display
 chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[name])
-[cc.cast_info.friendly_name for cc in chromecasts]
 
 cast = chromecasts[0]
 cast.wait()
 
 mc = cast.media_controller
-mc.play_media("test.mp3", "audio/mp3")
+mc.play_media(f"{base_url}/test.mp3", "audio/mp3")
 mc.block_until_active()
