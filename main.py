1688630457ab2f1d5410e61c909346353d1c4472
Eero Vilpponen <eero.vilpponen@gmail.com>
2025-11-06 07:58:09 +0200

    Nice

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import pychromecast
 import dotenv
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Play audio on Chromecast device.")
+
+# Positional arg, text to play
+parser.add_argument("text", type=str, help="Text to play on Chromecast")
+
+args = parser.parse_args()
+
 
 dotenv.load_dotenv()
 
@@ -12,6 +22,11 @@ chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[name]
 cast = chromecasts[0]
 cast.wait()
 
+# Replace spaces with _ and lowercase the text to form the filename
+filename = args.text.replace(" ", "_").lower() + ".mp3"
+
+print(f"Playing {filename} on {name} Chromecast")
+
 mc = cast.media_controller
-mc.play_media(f"{base_url}/test.mp3", "audio/mp3")
+mc.play_media(f"{base_url}/{filename}", "audio/mp3")
 mc.block_until_active()
